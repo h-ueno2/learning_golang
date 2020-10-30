dee920371f87a3201695533636ba4492196cd66a
h-ueno2 <menbou1212@gmail.com>
2020-10-30 21:58:28 +0900

    methods and pointer indirection(2)

---
+++ 04_Methods_and_interfaces/07_methods-and-pointer-indirection-2/indirection-values.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+// レシーバの変数を変更する場合はポインタレシーバにする必要がある
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func AbsFunc(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+	fmt.Println(AbsFunc(v))
+
+	p := &Vertex{4, 3}
+	fmt.Println(p.Abs())
+	fmt.Println(AbsFunc(*p))
+}
