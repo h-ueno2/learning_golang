0f6c1c2a7de5e251cb6faa536b815f55a057954a
h-ueno2 <menbou1212@gmail.com>
2020-10-31 18:27:51 +0900

    Exercise:Reader

---
+++ 04_Methods_and_interfaces/22_exercise-reader/exercise-reader.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type MyReader struct{}
+
+func (m MyReader) Read(rb []byte) (n int, e error) {
+	for n, e = 0, nil; n < len(rb); n++ {
+		rb[n] = 'A'
+	}
+	return n, e
+}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+
+func main() {
+	m := MyReader{}
+	b := make([]byte, 1024, 2048)
+	m.Read(b)
+	fmt.Println(fmt.Sprintf("%s", b))
+}
