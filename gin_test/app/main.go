f2525f961666872f2effc4c23c61252f2f275ab5
h-ueno2 <menbou1212@gmail.com>
2020-11-01 17:41:57 +0900

    HTMLの例

--- gin_test/app/main.go
+++ gin_test/app/main.go
@@ -15,11 +15,11 @@ func main() {
 		ua = c.GetHeader("User-Agent")
 		c.Next()
 	})
+	engine.LoadHTMLGlob("templates/*")
 
 	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message":    "hello world",
-			"User-Agent": ua,
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"message": "hello gin",
 		})
 	})
 	engine.Run(":3000")
