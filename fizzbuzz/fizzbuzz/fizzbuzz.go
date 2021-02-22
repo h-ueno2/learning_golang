1c0a352fcd6a553feb9134a7b725c7a5d1c7d3d3
h-ueno2 <menbou1212@gmail.com>
2021-02-22 22:00:32 +0900

    定数化

--- fizzbuzz/fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz/fizzbuzz.go
@@ -2,13 +2,19 @@ package fizzbuzz
 
 import "fmt"
 
+const (
+	fizzbuzz string = "FizzBuzz"
+	fizz     string = "Fizz"
+	buzz     string = "Buzz"
+)
+
 func Answer(i int) string {
 	if i%15 == 0 {
-		return "FizzBuzz"
+		return fizzbuzz
 	} else if i%3 == 0 {
-		return "Fizz"
+		return fizz
 	} else if i%5 == 0 {
-		return "Buzz"
+		return buzz
 	}
 	return fmt.Sprint(i)
 }
